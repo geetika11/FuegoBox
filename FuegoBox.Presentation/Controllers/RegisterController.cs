67523a0a22dcebf2cd35091699cb4f0334385c68
geetika11 <geetika.varshney@nagarro.com>
2018-11-29 09:39:01 +0530

    initial commit(prepared initial skeleton)

---
+++ FuegoBox.Presentation/Controllers/RegisterController.cs
@@ -0,0 +1,38 @@
+﻿using FuegoBox.Presentation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FuegoBox.Presentation.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+               return RedirectToAction("Index", "Home");
+                
+               
+            
+
+            // If we got this far, something failed, redisplay form
+           
+        }
+
+
+
+
+    }
+}
---
+++ FuegoBox.Presentation/Models/RegisterModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FuegoBox.Presentation.Models
+{
+    public class RegisterModel
+    {
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+       
+        public string EmailId { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        
+        public string Password { get; set; }
+
+
+        [Required]
+        [RegularExpression("^[a-zA-Z ]+", ErrorMessage = "Name may only contain alphabetic characters and space")]
+       
+        public string Name { get; set; }
+
+
+        [Required]
+        [RegularExpression(@"\d{10}", ErrorMessage = "Please enter a valid Mobile Number")]
+       
+        public string PhoneNo { get; set; }
+
+
+    }
+}
